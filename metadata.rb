03d0ddeea417bc213a9c917031ece346fc57723e
Anton Butovich <abutovich@gmail.com>
2014-11-10 16:22:41 +0200

    fix version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email 'abutovich@qubell.com'
 license          'All rights reserved'
 description	 'monkeypathching chef resources on compile time'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          'v0.0.6'
+version          '0.0.6'
